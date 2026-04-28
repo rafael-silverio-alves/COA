3ead5a62f56b0520060a5fcf4f05603e6b39eda5
rafael-silverio-alves <rafael.silverio@igarashi.com.br>
2026-04-28 10:16:10 -0300

    Corrige layout responsivo para Android

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2964275083"
+    serviceWorkerVersion: "1900342509"
   }
 });
