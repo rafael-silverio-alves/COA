2dfdea12551c33891ee7f892a3948450dbb3c0eb
rafael-silverio-alves <rafael.silverio@igarashi.com.br>
2026-07-24 10:04:59 -0300

    Deploy: Publicação completa versão local

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "60a338ec88cb07292a1487b319a13b09
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2a0a0601ec7cbe0f07fa7f50bad77c9e",
+"flutter_bootstrap.js": "5139d4512678612d79f1fa08d9b91bfd",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
