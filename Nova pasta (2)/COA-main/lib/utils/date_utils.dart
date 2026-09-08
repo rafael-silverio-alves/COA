5b3f8c44c3320d1ea8f120d56560ef2b798543d7
rafael-silverio-alves <rafael.silverio@igarashi.com.br>
2026-09-08 10:01:43 -0300

    Republicação completa versão local

--- Nova pasta (2)/COA-main/lib/utils/date_utils.dart
+++
@@ -1,7 +0,0 @@
-class DateUtilsBR {
-  static String format(DateTime d) {
-    return "${d.day.toString().padLeft(2, '0')}/"
-        "${d.month.toString().padLeft(2, '0')}/"
-        "${d.year}";
-  }
-}
